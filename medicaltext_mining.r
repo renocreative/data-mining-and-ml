1656c816a927cd08183f187ada0a81e9479971a1
Renaud <renaudbougues@users.noreply.github.com>
2016-08-09 00:09:56 -0400

    Update medicaltext_mining.r

--- medicaltext_mining.r
+++ medicaltext_mining.r
@@ -1,5 +1,8 @@
 # Text Classification Problem
 
+library(RWeka)
+library(pROC)
+
 # Pre-processing
 # Data Representation & Feature selection
 estrogens_dataset <- read.csv("units_Estrogens.txt",header=FALSE)
@@ -35,14 +38,24 @@ estrogens_dataset[, fctr.cols] <- sapply(estrogens_dataset[, fctr.cols], as.char
 estrogens_dataset = c(estrogens_dataset[,2:5], estrogens_dataset[,1])
 
 #weka.filters.unsupervised.attribute.StringToWordVector –R 2-3,5 –W 10000 –prune-rate -1.0 –C –N 0 –L –S 
+
 #–stemmer weka.core.stemmers.NullStemmer –M 1 
+LovinsStemmer(estrogens_dataset, control = NULL)
 #–tokenizer “weka.core.tokenizers.WordTokenizer –delimiters \” \\r\\n\\t.,;:\\\’\\\”()?!\””
+estrogens_dataset <- WordTokenizer(estrogens_dataset, control = NULL)
 
 
 #AttributeEvaluator = weka.attributeSelection.InfoGainAttributeEval
 InfoGainAttributeEval(formula, data, subset, na.action, control = NULL)
 
 #SearchMethod = weka.attributeSelection.Ranker -T 0 -N -1
+nombi <-
+> make_Weka_filter("weka/filters/supervised/attribute/AttributeSelection")
+datbin<- nombi(gene ~., data=X1X2X3X4W,  control =Weka_control(
+> S=list("weka.attributeSelection.GeneticSearch"),
+> E=list("weka.attributeSelection.WrapperSubsetEval"),B =
+> list("weka.classifiers.trees.RandomForest -F 5 -T 0.01 -R 1 -- -I 10 -K
+> 0 -S 1")))
 
 #weka.filters.unsupervised.attribute.Remove -V –R
 
@@ -51,9 +64,41 @@ InfoGainAttributeEval(formula, data, subset, na.action, control = NULL)
 
 # Model learning
 # 5x2 cross validation with NaiveBayes, SMO, J48, IBk, RandomForest, AdaBoostM1[w/ NaiveBayes]. 
+# WOW("weka/classifiers/bayes/NaiveBayes")
 
+# LazyBayes
+estrogens_bayes = LBR(K ~ ., data = estrogens_dataset)
+eval_bayes <- evaluate_Weka_classifier(estrogens_bayes, numFolds = 5, repeats = 2, complexity = FALSE, seed = 1, class = TRUE)
 
-# We should get:
+# SMO
+estrogens_smo = SMO(K ~ ., data = estrogens_dataset, control = Weka_control(K = list("weka.classifiers.functions.supportVector.RBFKernel", G = 2)))
+eval_smo <- evaluate_Weka_classifier(estrogens_smo, numFolds = 5, repeats = 2, complexity = FALSE, seed = 1, class = TRUE)
+
+# J48
+estrogens_j48 <- J48(K ~ ., data = estrogens_dataset)
+eval_j48 <- evaluate_Weka_classifier(estrogens_j48, numFolds = 5, repeats = 2, complexity = FALSE, seed = 1, class = TRUE)
+
+#IBk
+estrogens_IBk <- IBk(K ~ ., data = estrogens_dataset)
+eval_IBk <- evaluate_Weka_classifier(estrogens_IBk, numFolds = 5, repeats = 2, complexity = FALSE, seed = 1, class = TRUE)
+
+#RandomForest
+#estrogens_forest <- make_Weka_classifier("weka/classifiers/trees/RandomForest")
+estrogens_forest <- Bagging(K ~ ., data = estrogens_dataset, control = Weka_control(W = "weka/classifiers/trees/RandomForest")
+eval_forest <- evaluate_Weka_classifier(estrogens_forest, numFolds = 5, repeats = 2, complexity = FALSE, seed = 1, class = TRUE)
+
+# AdaBoostM1
+estrogens_m1 <- AdaBoostM1(K ~ ., data = estrogens_dataset, control = Weka_control(W = list(LBR, M = 30)))
+eval_m1 <- evaluate_Weka_classifier(estrogens_m1, numFolds = 5, repeats = 2, complexity = FALSE, seed = 1, class = TRUE)
+
+str(multiclass.roc(estrogens_dataset$K, eval_bayes))
+str(multiclass.roc(estrogens_dataset$K, eval_smo))
+str(multiclass.roc(estrogens_dataset$K, eval_j48))
+str(multiclass.roc(estrogens_dataset$K, eval_IBk))
+str(multiclass.roc(estrogens_dataset$K, eval_forest))
+str(multiclass.roc(estrogens_dataset$K, eval_m1))
+
+# We should get approx.:
 # Classifier	AUC (On Average)
 # NaiveBayes	0.924783
 # SMO	0.7192361
